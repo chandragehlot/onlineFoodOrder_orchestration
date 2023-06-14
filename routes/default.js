75b520ae3d72a421f7559675364a9ffc9c14b290
chandragehlot <chkl2401@gmail.com>
2023-06-14 14:32:13 +0530

    Update default.js

--- routes/default.js
+++ routes/default.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const defautController = require('../controller/defaultController');
-const { verifyAuth } = require('../middlewares');
-const router = express.Router();
+// const express = require('express');
+// const defautController = require('../controller/defaultController');
+// const { verifyAuth } = require('../middlewares');
+// const router = express.Router();
 
-router.get("/default", verifyAuth, defautController.baseFunc);
-router.post('/testBulkRecords', defautController.testBulkRecordsHanlder);
+// router.get("/default", verifyAuth, defautController.baseFunc);
+// router.post('/testBulkRecords', defautController.testBulkRecordsHanlder);
 
-module.exports = router;
+// module.exports = router;
