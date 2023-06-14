58148a1579fcfea956789b491343f7fd0eddf240
chandragehlot <chkl2401@gmail.com>
2023-06-14 14:15:16 +0530

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,20 +1,20 @@
-const { verifyJWTToken } = require('../utils/bcrypt-util')
+// const { verifyJWTToken } = require('../utils/bcrypt-util')
 
-function verifyAuth(req, res, next) {
-    const jwtToken = req.body.token || req.query.token || req.headers["x-access-token"];
-    if (!jwtToken) {
-        throw Error("A token is required for authentication");
-    }
-    try {
-        const decodedInfo = verifyJWTToken(jwtToken);
-        req.user = decodedInfo;
-    } catch (error) {
-        console.log(error);
-        throw Error("invalid token")
-    }
-    return next();
-}
+// function verifyAuth(req, res, next) {
+//     const jwtToken = req.body.token || req.query.token || req.headers["x-access-token"];
+//     if (!jwtToken) {
+//         throw Error("A token is required for authentication");
+//     }
+//     try {
+//         const decodedInfo = verifyJWTToken(jwtToken);
+//         req.user = decodedInfo;
+//     } catch (error) {
+//         console.log(error);
+//         throw Error("invalid token")
+//     }
+//     return next();
+// }
 
-module.exports = {
-    verifyAuth
-}
+// module.exports = {
+//     verifyAuth
+// }
