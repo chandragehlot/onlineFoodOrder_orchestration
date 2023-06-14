7da95a28576c5ff870d6c833de70a7f2cb4577b1
chandragehlot <chkl2401@gmail.com>
2023-06-14 14:31:26 +0530

    Delete account.js

--- routes/account.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const AccountController = require('../controller/accountController');
-
-// const router = express.Router();
-
-// router.post("/signup", AccountController.signUp);
-// router.post("/login", AccountController.login);
-
-// module.exports = router;
