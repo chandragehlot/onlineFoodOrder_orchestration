e9ffd77887d3f6046a96b8bba23df5322d812784
chandragehlot <chkl2401@gmail.com>
2023-06-14 14:14:22 +0530

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const AccountController = require('../controller/accountController');
+// const express = require('express');
+// const AccountController = require('../controller/accountController');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/signup", AccountController.signUp);
-router.post("/login", AccountController.login);
+// router.post("/signup", AccountController.signUp);
+// router.post("/login", AccountController.login);
 
-module.exports = router;
+// module.exports = router;
